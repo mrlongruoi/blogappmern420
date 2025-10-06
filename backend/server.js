aa76f2bdf5de914e54098ea1f50909bbc3fbc6f2
mrlongruoi <mrlong01012020@gmail.com>
2025-10-06 23:18:47 +0700

    fix01

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
-
 const authRoutes = require("./routes/authRoutes");
 const blogPostRoutes = require("./routes/blogPostRoutes");
 const commentRoutes = require("./routes/commentRoutes");
@@ -27,17 +26,20 @@ connectDB();
 // Middleware
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Blog API!");
+});
+
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", blogPostRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/api/dashboard-summary", dashboardRoutes);
-
 app.use("/api/ai", aiRoutes);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
-// Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//Start Server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
