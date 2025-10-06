8f2a7a93a0175cbd86cb4a4768cd1b780da54fe7
mrlongruoi <mrlong01012020@gmail.com>
2025-10-07 00:08:15 +0700

    fixxxxx01

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -12,14 +12,15 @@ router.post("/login", loginUser);         // Login User
 router.get("/profile", protect, getUserProfile);  // Get User Profile
 
 
+// === SỬA LẠI ROUTE NÀY ===
 router.post("/upload-image", upload.single("image"), (req, res) => {
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded" });
   }
-  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-    req.file.filename
-  }`;
-  res.status(200).json({ imageUrl });
+  
+  // Lấy trực tiếp URL đầy đủ từ Cloudinary qua req.file.path
+  // và gửi về cho frontend
+  res.status(200).json({ imageUrl: req.file.path });
 });
 
 module.exports = router;
